08708bdaeb997bc1c3cf5b3f702d966683c5e518
Koala <32124652+Koala0107@users.noreply.github.com>
2026-09-09 05:38:41 +0900

    Add item editor block

---
+++ CrouchLock/src/main/java/kr/koala/crouchlock/ItemEditorBlock.java
@@ -0,0 +1,29 @@
+package kr.koala.crouchlock;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public final class ItemEditorBlock extends Block {
+    public static final MapCodec<ItemEditorBlock> CODEC = createCodec(ItemEditorBlock::new);
+
+    public ItemEditorBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected MapCodec<? extends Block> getCodec() {
+        return CODEC;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        return ActionResult.SUCCESS;
+    }
+}
