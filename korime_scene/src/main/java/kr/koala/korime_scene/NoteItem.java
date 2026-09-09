1b448526286b70cb6efefa9b7617cfbced02e830
Koala <32124652+Koala0107@users.noreply.github.com>
2026-09-10 03:11:42 +0900

    Add note item

---
+++ korime_scene/src/main/java/kr/koala/korime_scene/NoteItem.java
@@ -0,0 +1,23 @@
+package kr.koala.korime_scene;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public final class NoteItem extends Item {
+    public NoteItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
+        String title = NoteData.getTitle(stack);
+        if (!title.isBlank()) tooltip.add(Text.literal(title).formatted(Formatting.YELLOW));
+        tooltip.add(Text.literal("우클릭하여 자유롭게 기록").formatted(Formatting.GRAY));
+    }
+}
